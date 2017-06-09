7d5550fc40db0b792955d0da787d2c826b7590ad
Lior Chamla <lchamla@gmail.com>
2017-06-09 15:19:23 +0200

    code: adding comments to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,20 +4,39 @@ import PropTypes from 'prop-types'
 import '../css/Home.css'
 import background from '../img/header.jpg'
 
+/**
+ * ReactComponent Home 
+ * Presents the website home page with a welcome text and a button to browse playlists
+ */
 class Home extends Component {
+  /**
+   * Surfacing the Router 
+   * @type {Object}
+   */
   static contextTypes = {
     router: PropTypes.object
   }
 
+  /**
+   * Managing the top navbar transparency
+   */
   componentDidMount(){
     document.querySelector('#navigation').classList.add('navbar-transparent');
   }
 
+  /**
+   * Routing user to the playlists index
+   * @param  {SyntheticEvent} e Click event
+   */
   goToPlaylists = (e) => {
     e.preventDefault();
     this.context.router.history.push('/playlists');
   }
 
+  /**
+   * Rendering homepage
+   * @return {JSX} The homepage template
+   */
   render () {
     return (
       <div className="page-header clear-filter" data-filter-color="orange">
@@ -27,9 +46,10 @@ class Home extends Component {
               <i className="fa fa-youtube" style={{marginBottom: '20px', fontSize: '10em'}}></i>
               <h1 className="h1-seo">YouTube Mix!</h1>
               <h3>A React App to create YouTube Playlists !</h3>
-              <a href="#!" onClick={(e) => this.goToPlaylists(e)} className="btn btn-lg btn-neutral" style={{fontSize: '2em'}} >
+              <a href="#!" onClick={(e) => this.goToPlaylists(e)} className="btn btn-lg btn-neutral" style={{fontSize: '2em', marginBottom: '20px'}} >
                 <i className="fa fa-headphones"></i>&nbsp; Browse playlists
               </a>
+              <p>This has no utility, it is just a project to practice React, React Router, YouTube IFrame API and Firebase</p>
           </div>
         </div>
       </div>
